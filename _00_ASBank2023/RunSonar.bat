6c181fae8073af6b570759ec913702472c79c2de
JulienMaa <julien.maaroufi@gmail.com>
2024-11-09 11:29:29 +0100

    Update RunSonar.bat

--- _00_ASBank2023/RunSonar.bat
+++ _00_ASBank2023/RunSonar.bat
@@ -1,1 +1,5 @@
-mvn clean verify sonar:sonar -Dsonar.projectKey=BANK_IUT_2023 -Dsonar.host.url=http://localhost:9000 -Dsonar.login=sqp_c55e85adf298ef5ffae40c13ec282dfc9482301b
+mvn clean verify sonar:sonar \
+  -Dsonar.projectKey=Qualdev_G7 \
+  -Dsonar.projectName='Qualdev_G7' \
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.token=sqp_b6652c42d58213d185df0030b0e0309455ded038
