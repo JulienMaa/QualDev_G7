393840835e0c2e6cc25eb6687b82a2171756c93d
JulienMaa <julien.maaroufi@gmail.com>
2025-01-20 17:28:11 +0100

    Docker, login fix
    Debug de la page login via docker

--- _00_ASBank2023/RunDocker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Stop and remove all containers, networks, and volumes created by docker-compose
-docker-compose down -v
-
-# Build and start the containers
-docker-compose up --build
--- _00_ASBank2023/script/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS bankiut;
-USE bankiut;
-
-SOURCE ./dumpSQL.sql;
-
-CREATE DATABASE IF NOT EXISTS bankiuttest;
-USE bankiuttest;
-
-SOURCE ./dumpSQL_JUnitTest.sql;
