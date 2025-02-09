6f5ad8338b2da6f0c8e2fb7413a4b6fa231ea74c
JulienMaa <julien.maaroufi@gmail.com>
2025-02-09 15:57:49 +0100

    Selenium testLogin
    Ajout d'un fichier config.properties pour ne pas afficher les infos de l'utilisateur dans le fichier test.
    
    Création d'un config reader pour obtenir les informations dans le fichier config.
    
    Création d'un testLogin pour vérifier que l'utilisateur puisse bien se connecter à la bdd.

--- _00_ASBank2023/src/test/java/com/iut/banque/test/selenium/SeleniumTest.java
+++ _00_ASBank2023/src/test/java/com/iut/banque/test/selenium/SeleniumTest.java
@@ -1,5 +1,6 @@
 package com.iut.banque.test.selenium;
 
+import com.iut.banque.test.utils.ConfigReader;
 import org.junit.Assert;
 import static org.junit.Assume.assumeTrue;
 import org.junit.Test;
@@ -15,13 +16,30 @@ import java.net.URL;
 
 public class SeleniumTest {
   private WebDriver driver;
+  private String baseUrl;
+  private String loginPage;
+  private String username;
+  private String password;
+  private String loginButtonName;
+  private String usernameFieldName;
+  private String passwordFieldName;
+  private String successElementId;
 
   @Before
   public void setUp() {
     System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
     driver = new ChromeDriver();
-
     driver.manage().window().setSize(new Dimension(842, 816));
+
+    // Charger les valeurs de config.properties
+    baseUrl = ConfigReader.getProperty("baseUrl");
+    loginPage = ConfigReader.getProperty("loginPage");
+    username = ConfigReader.getProperty("username");
+    password = ConfigReader.getProperty("password");
+    loginButtonName = ConfigReader.getProperty("loginButtonName");
+    usernameFieldName = ConfigReader.getProperty("usernameFieldName");
+    passwordFieldName = ConfigReader.getProperty("passwordFieldName");
+    successElementId = ConfigReader.getProperty("successElementId");
   }
 
   @After
@@ -31,7 +49,7 @@ public class SeleniumTest {
 
   @Test
   public void testLogoExist() {
-    String url = "http://localhost:8081/_00_ASBank2023/";
+    String url = baseUrl;
 
     assumeTrue("Skipping test as URL is unreachable", urlExists(url));
 
@@ -40,15 +58,35 @@ public class SeleniumTest {
     Assert.assertNotNull(logo);
   }
 
+  @Test
+  public void testLogin() {
+    // Aller à la page de connexion
+    driver.get(baseUrl + loginPage);
+
+    // Vérifier si la page s'est bien chargée
+    assumeTrue("Skipping test as URL is unreachable", urlExists(baseUrl + loginPage));
+
+    // Remplir le formulaire de connexion
+    WebElement usernameInput = driver.findElement(By.name(usernameFieldName));
+    WebElement passwordInput = driver.findElement(By.name(passwordFieldName));
+    WebElement loginButton = driver.findElement(By.name(loginButtonName));
+
+    usernameInput.sendKeys(username);
+    passwordInput.sendKeys(password);
+    loginButton.click();
+
+    // Vérifier si la connexion a réussi en recherchant un élément spécifique sur la page
+    WebElement successElement = driver.findElement(By.id(successElementId));
+    Assert.assertNotNull("Connexion échouée, l'élément de confirmation n'a pas été trouvé", successElement);
+  }
+
   public boolean urlExists(String url) {
     try {
-      // Connect to the web page
       HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
       connection.setRequestMethod("HEAD");
       connection.setConnectTimeout(3000);
       connection.setReadTimeout(3000);
 
-      // Get the code returned by the page
       int responseCode = connection.getResponseCode();
       return (responseCode == 200);
     } catch (Exception e) {
